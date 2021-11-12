1774e8190c973e5f3a19a100522ee372fcd61210
perogeremmer <mhudyaramadhana@gmail.com>
2021-11-12 13:08:15 +0700

    First Commit

---
+++ register.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include('header.php') ?>
+</head>
+
+<body>
+    <?php include('navbar.php') ?>
+
+    <main role="main " class="container">
+        <?php include('welcome_message.php') ?>
+
+        <div class="container mt-5">
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h5 class="mb-4">Halaman Register</h5>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include('footer.php') ?>
+
+    <?php include('scripts.php') ?>
+</body>
+
+</html>
